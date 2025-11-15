1a2209b38229c84fef2c2ac88022f57773cd1162
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-15 14:57:25 +0100

    added a new file

---
+++ src/components/ui/NewBranchFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBranchFile = () => {
+  return (
+    <div>
+      <h1>This is a new file created in a new branch.</h1>
+    </div>
+  )
+}
+
+export default NewBranchFile
