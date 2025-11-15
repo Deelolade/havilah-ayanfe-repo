51dfad422d049c8d64bd6d8334d87fd6a83b0a25
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-11-15 15:06:39 +0100

    newly added

--- src/components/ui/NewBranchFile.jsx
+++ src/components/ui/NewBranchFile.jsx
@@ -4,6 +4,7 @@ const NewBranchFile = () => {
   return (
     <div>
       <h1>This is a new file created in a new branch.</h1>
+      <p>This file is used to test our knowledge of git commands</p>
     </div>
   )
 }
